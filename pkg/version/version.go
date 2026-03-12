f85da81555d6dbcc8b7105aad77c9d1b51cb8544
Charles <charleswhitesun@gmail.com>
2026-03-12 22:07:08 +0800

    fix(version) 調整建構說明的格式，改善可讀性

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -1,6 +1,7 @@
 // Package version holds the build-time version information injected via ldflags.
 // Build with:
-//   go build -ldflags "-X go-pathprobe/pkg/version.Version=v1.2.3-abc123"
+//
+//	go build -ldflags "-X go-pathprobe/pkg/version.Version=v1.2.3-abc123"
 //
 // The Version string follows the format: <tag>-<6-char-git-hash>[-dirty]
 // where "-dirty" indicates uncommitted changes or unpushed commits.
